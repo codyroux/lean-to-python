d70e60bb1d6680bdef6121b506d938c239d22911
Cody Roux <cody.roux@gmail.com>
2025-12-22 22:31:31 -0800

    Fooling around

--- PythonModlang/Basic.lean
+++ PythonModlang/Basic.lean
@@ -1,16 +1,17 @@
 import Lean.Meta
 import Lean.Elab
 import Lean.Expr
+import Qq
 
 structure Signature (S : Type) where
   arity : S → Nat
-  argTy {s : S} : Fin (arity s) → Type
+  argTy (s : S) : Fin (arity s) → Type
   outTy (s : S) : Type
 
 inductive FreeM {S : Type} (σ : Signature S) : Type → Type 1 where
 | fPure : α → FreeM σ α
 | fBind : FreeM σ α → (α → FreeM σ β) → FreeM σ β
-| fOp (s : S) : (args : ∀ i : Fin (σ.arity s), FreeM σ (σ.argTy i)) →
+| fOp (s : S) : (args : ∀ i : Fin (σ.arity s), σ.argTy _ i) →
                 FreeM σ (σ.outTy s)
 
 instance : Monad (FreeM σ) where
@@ -18,7 +19,7 @@ instance : Monad (FreeM σ) where
   bind := FreeM.fBind
 
 structure Env {S : Type} (σ : Signature S) (m : Type → Type) where
-  evalS (s : S) : (∀ i : Fin (σ.arity s), m (σ.argTy i)) → m (σ.outTy s)
+  evalS (s : S) : (∀ i : Fin (σ.arity s), σ.argTy _ i) → m (σ.outTy s)
 
 
 open FreeM
@@ -30,12 +31,12 @@ match x with
 | fBind x f => do
   let a ← evalFreeM env x
   evalFreeM env (f a)
-| fOp s f => env.evalS s (fun i => evalFreeM env (f i))
+| fOp s f => env.evalS s (fun i => f i)
 
 
-def iNil (n : Fin 0) : α := by cases n; omega
+def iNil {α : Type u} (n : Fin 0) : α := by cases n; omega
 
-def iCons (a : α) (v : Fin n → α) : Fin (n+1) → α := Fin.cases a (fun j => v j)
+def iCons {α : Type u} {n : Nat} (a : α) (v : Fin n → α) : Fin (n+1) → α := Fin.cases a (fun j => v j)
 
 def listToVec (l : List α) : Fin (l.length) → α :=
 match l with
@@ -51,7 +52,41 @@ open Lean in
 macro_rules
   | `(⟦ $elems,* ⟧) => `(listToVec [$elems,*])
 
-#check ⟦1, 2 , 3⟧
+#check ⟦1, 2, 3⟧
+
+section Test
+
+inductive Commands where
+| read
+| write
+
+def RWSig : Signature Commands where
+  arity
+    | .read => 0
+    | .write => 1
+  argTy c _ :=
+  match c with
+  | .write => Nat
+  outTy c :=
+  match c with
+  | .read => Nat
+  | .write => Unit
+
+#print ULift
+
+--- whyyyy
+def read : FreeM RWSig Nat := FreeM.fOp Commands.read (fun i => ⟦⟧ i)
+
+def write : Nat → FreeM RWSig Unit := fun n => FreeM.fOp Commands.write ⟦n⟧
+
+#check ⟦⟧
+#check do
+  let x ← pure 4
+  let y : Nat ← read
+  let _ ← write 3
+  return x + y
+
+end Test
 
 open Lean
 open Std.Format
@@ -78,7 +113,7 @@ private def f : Nat → Nat := fun n => n+1
 
 #eval do
   -- let t ← `(if true then 3 else 4)
-  let t ← `(3 = x)
+  let t ← `(x * x)
   let e ← Elab.Term.elabTerm t none
   let e ← instantiateMVars e
   logInfo m!"{t} -> {e}"
@@ -113,24 +148,18 @@ mutual
 partial def addToPython (e : Lean.Expr) : MetaM PyVal :=
   if e.getAppFn.constName! == ``HAdd.hAdd then
     do
-      dbg_trace s!"{e}"
-      dbg_trace s!"{e.getArg! 4}"
       let arg₁ ← exprToPython <| e.getArg! 4
-      dbg_trace s!"{e.getArg! 5}"
       let arg₂ ← exprToPython <| e.getArg! 5
-      return paren <| arg₁ ++ " + " ++ arg₂
+      return s!"({arg₁} + {arg₂})"
   else
   failure
 
 partial def mulToPython (e : Lean.Expr) : MetaM PyVal :=
   if e.getAppFn.constName! == ``HMul.hMul then
     do
-      dbg_trace s!"{e}"
-      dbg_trace s!"{e.getArg! 4}"
       let arg₁ ← exprToPython <| e.getArg! 4
-      dbg_trace s!"{e.getArg! 5}"
       let arg₂ ← exprToPython <| e.getArg! 5
-      return paren <| arg₁ ++ " * " ++ arg₂
+      return s!"({arg₁} * {arg₂})"
   else
   failure
 
@@ -138,12 +167,20 @@ partial def mulToPython (e : Lean.Expr) : MetaM PyVal :=
 partial def eqToPython (e : Lean.Expr) : MetaM PyVal :=
   if e.getAppFn.constName! == ``Eq then
     do
-      dbg_trace s!"{e}"
-      dbg_trace s!"{e.getArg! 1}"
       let arg₁ ← exprToPython <| e.getArg! 1
-      dbg_trace s!"{e.getArg! 5}"
       let arg₂ ← exprToPython <| e.getArg! 5
-      return paren <| arg₁ ++ " * " ++ arg₂
+      return s!"({arg₁} == {arg₂})"
+  else
+  failure
+
+
+partial def iteToPython (e : Lean.Expr) : MetaM PyVal :=
+  if e.getAppFn.constName! == ``ite then
+    do
+      let arg₁ ← exprToPython <| e.getArg! 1
+      let arg₂ ← exprToPython <| e.getArg! 3
+      let arg₃ ← exprToPython <| e.getArg! 4
+      return s!"({arg₂} if {arg₁} else {arg₃})"
   else
   failure
 
@@ -164,14 +201,16 @@ partial def exprToPython (e : Lean.Expr) : MetaM PyVal := do
       let n := e.getAppFn.constName!
       if n == ``OfNat.ofNat then numToPython e
       else if n == ``HAdd.hAdd then addToPython e
+      else if n == ``HMul.hMul then mulToPython e
       else if n == ``Eq then eqToPython e
+      else if n == ``ite then iteToPython e
       else funToPython e
     else failure
 
 end
 
 #eval do
-  let t ← `(f (0 + x))
+  let t ← `(if 3 = 3 then 0 else f (0 * x))
   let e ← Elab.Term.elabTerm t none
   let e ← instantiateMVars e
   logInfo s!"{e}"
@@ -235,4 +274,27 @@ def evalFreeM [Monad m] (x : FreeM α) (env : EvalEnv m α) : OptionT m α :=
     | .isTrue p => evalFreeM (x p) e
     | _ => failure)
   | _, _ => failure
+ -/
+
+/-
+-- I don't get how any of this works
+section TestQ
+open Qq
+
+def foo (x : Q(Nat)) : MetaM String :=
+  match x with
+  | ~q(0) => return "foo"
+  | ~q($x) => return s!"{x}"
+  | _ => return "bar"
+
+abbrev y : Nat := 0
+
+#eval do
+  let t ← `(y)
+  let e ← Elab.Term.elabTerm t none
+  let bar ← foo e
+  logInfo m!"{bar}"
+
+
+end TestQ
  -/
